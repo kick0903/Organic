e14935ecd4ed0faa735fa09990f053e5ae4b48be
kick0903 <kick0903@gmail.com>
2022-10-08 09:24:35 +0700

    fix

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -15,31 +15,33 @@ const Products = () => {
             <div className="breadbox">
             <a href="/">Home</a>/<text>Products</text>
             </div>
-            <div className="grid">
-            <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" />
-            <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" />
-            <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" />
-            <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" />
-            <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" />
-            <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" />
-            <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" />
-            <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" />
-            <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" />
-            <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" />
-            <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" />
-            <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" />
-            <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" />
-            <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" />
-            <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" />
-            <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" />
-            <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" />
-            <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" />
-            <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" />
-            <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" />
-            <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" />
-            <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" />
-            <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" />
-            <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" />
+            <div className="container">
+            <div className="row">
+           <div className="col-md-3"> <ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" /></div>
+           <div className="col-md-3"> <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" /></div>
+           <div className="col-md-3"> <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" /></div>
+           <div className="col-md-3"> <ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" /></div>
+           <div className="col-md-3"><ProductsCard img={img1} cardtitle="Organic peanut oil" price="26.000 VND" /></div>
+           <div className="col-md-3"> <ProductsCard img={img2} cardtitle="Organic ginger" price="10.000 VND" /></div>
+            <div className="col-md-3"> <ProductsCard img={img3} cardtitle="Dried bamboo shoots" price="30.000 VND" /></div>
+            <div className="col-md-3"><ProductsCard img={img4} cardtitle="Dried skullcap" price="20.000 VND" /></div>
+            </div>
             </div>
         </div>
     );
